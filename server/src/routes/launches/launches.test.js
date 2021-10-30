83bb66953022393ac31f684149903bd711fcd487
Amir Halloul <amirhalloul@gmail.com>
2021-10-30 16:54:01 +0100

    Add API versioning to the API tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,6 +3,8 @@ const app = require('../../app');
 
 const { connectDb, disconnectDb } = require('../../services/mongo');
 
+const API_VERSION = 'v1'
+
 describe('Test Launches API', () => {
     beforeAll(async () => {
         await connectDb();
@@ -15,7 +17,7 @@ describe('Test Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get(`/${API_VERSION}/launches`)
                 .expect('Content-Type', /json/)
                 .expect(200);
         });
@@ -44,7 +46,7 @@ describe('Test Launches API', () => {
     
         test('It should respond with 201 created', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post(`/${API_VERSION}/launches`)
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -58,7 +60,7 @@ describe('Test Launches API', () => {
     
         test('It should catch missing required properties', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post(`/${API_VERSION}/launches`)
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -70,7 +72,7 @@ describe('Test Launches API', () => {
     
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post(`/${API_VERSION}/launches`)
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
