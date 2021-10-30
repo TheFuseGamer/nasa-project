1bac0cd52be147334ae7ce08ace486c094ef8a81
Amir Halloul <amirhalloul@gmail.com>
2021-10-30 13:51:58 +0100

    Install mongoose and create mongoose models

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+        default: 0
+    },
+    mission: {
+        type: String,
+        required: true
+    },
+    rocket: {
+        type: String,
+        required: true
+    },
+    launchDate: {
+        type: Date,
+        required: true
+    },
+    target: {
+        type: String,
+        ref: 'Planet',
+        required: true
+    },
+    customers: {
+        type: [String]
+    },
+    upComing: {
+        type: Boolean,
+        required: true
+    },
+    success: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+module.exports = mongoose.model('Launch', launchesSchema);
